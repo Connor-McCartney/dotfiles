546584c00b0a5919fbecea4b217fcd352a999a7d
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-09-07 08:29:17 +1000

    Update main.sh

--- .bashrc.d/main.sh
+++ .bashrc.d/main.sh
@@ -6,3 +6,8 @@ alias l=ls
 #normal/default green, directories turquoise underlined, executables red bold, .py yellow
 export LS_COLORS="no=32:di=4;96:ex=1;31:*.py=93"
 export PS1="\[\e[0;35m\][\u@\h \W]\[\[\e[m\]\[\e[0;37m\]\$\[\e[m\] \[\e[32m\]"
+
+
+c() {
+  cd "$1" && ls
+}
