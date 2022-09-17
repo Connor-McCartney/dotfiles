2f80619839bd60a1d8c429f1a1be751dcc0ba7ba
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-09-17 23:57:55 +1000

    Update set.lua

--- .config/nvim/lua/requireable/set.lua
+++ .config/nvim/lua/requireable/set.lua
@@ -4,7 +4,7 @@ vim.softtapstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.termguicolors = true 
+vim.opt.termguicolors = true
 vim.opt.guicursor = ""        --match terminal cursor
 vim.opt.nu = true             --line numbers
 vim.opt.mouse = "a"           --mouse support
@@ -12,6 +12,5 @@ vim.opt.hlsearch = false      --disable search highlighting
 vim.opt.wrap = false          --disable line wrapping
 vim.opt.scrolloff = 10        --line from top/bottom until scrolling
 
-vim.api.nvim_command([[
-autocmd BufRead,BufNewFile *.sage setfiletype python
-]])
+-- disable history popup
+vim.cmd("nnoremap q: <nop>")
