5ec0361a559742c224b030af9eee09452649d690
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-10-05 16:37:52 +1000

    Update set.lua

--- .config/nvim/lua/requireable/set.lua
+++ .config/nvim/lua/requireable/set.lua
@@ -3,6 +3,7 @@ vim.opt.tabstop = 4
 vim.softtapstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.termguicolors = true
 vim.opt.guicursor = ""        --match terminal cursor
