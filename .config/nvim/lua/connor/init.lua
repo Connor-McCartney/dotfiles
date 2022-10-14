b16d72453b59c058de4f43b0b74e1e6fc3fbd9e1
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-10-14 23:44:03 +1000

    Update init.lua

--- .config/nvim/lua/connor/init.lua
+++ .config/nvim/lua/connor/init.lua
@@ -3,24 +3,44 @@ require('connor.packer')
 local Remap = require('connor.keymap')
 local vnoremap = Remap.vnoremap
 
--- moves lines up/down keymap
+-- MOVES LINES UP/DOWN KEYMAP
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '<-2<CR>gv=gv")
 
--- LSP installer 
+-- LSP
 require("nvim-lsp-installer").setup {}
 local lspconfig = require("lspconfig")
 
-lspconfig.sumneko_lua.setup {}
-lspconfig.pyright.setup {}
-lspconfig.gopls.setup {}
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+lspconfig['sumneko_lua'].setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim', 'use'},
+      },
+    },
+  },
+}
+
+lspconfig['pylsp'].setup {
+  capabilities = capabilities
+}
+lspconfig['gopls'].setup {
+  capabilities = capabilities
+}
+lspconfig['ltex'].setup {
+  capabilities = capabilities
+}
+
 
 -- nvim-cmp
 local cmp = require'cmp'
 cmp.setup({
 snippet = {
    expand = function(args)
-     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+     require('luasnip').lsp_expand(args.body)
    end,
 },
   mapping = {
@@ -37,31 +57,8 @@ snippet = {
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
+    { name = 'luasnip' },
   }, {
     { name = 'buffer' },
   })
 })
-
-
--- Language Servers
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['sumneko_lua'].setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim', 'use'},
-      },
-    },
-  },
-}
-
-require('lspconfig')['pylsp'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['gopls'].setup {
-  capabilities = capabilities
-}
-
