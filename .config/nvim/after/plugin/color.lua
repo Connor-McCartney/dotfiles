96467293d19e7f6c187eb4299a6a3552d38d65b4
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-10-29 12:52:57 +1000

    Update color.lua

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,3 +1,5 @@
-vim.g.sonokai_transparent_background = true
+-- https://github.com/sainnhe/sonokai/blob/master/doc/sonokai.txt
+vim.g.sonokai_transparent_background = false
 vim.g.sonokai_disable_italic_comment = true
+vim.g.sonokai_style = 'atlantis'
 vim.cmd("colorscheme sonokai")
