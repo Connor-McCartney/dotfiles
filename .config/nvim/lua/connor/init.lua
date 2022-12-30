b119e810e8f6baa31ae085d05bed6d05d1a4ecb5
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2022-12-31 08:50:19 +1000

    Update init.lua

--- .config/nvim/lua/connor/init.lua
+++ .config/nvim/lua/connor/init.lua
@@ -1,3 +1,11 @@
 require('connor.set')
 require('connor.packer')
 require('connor.remap')
+
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    -- :TSUpdate
+    -- :TSInstall python
+    enable = {'python'},
+  },
+}
