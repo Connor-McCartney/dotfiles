a57e338180de67ffed24e49a0c05227c0bba6ef7
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-01-24 10:05:23 +1000

    Update main.sh

--- .bashrc.d/main.sh
+++ .bashrc.d/main.sh
@@ -3,14 +3,14 @@
 alias des="cd ~/Desktop"
 alias v=nvim
 alias ls="ls --color=auto"
-alias l="ls --color=auto"
+alias l=ls
 
 #normal/default green, directories aqua underlined, executables pink, .py orange
 export LS_COLORS="no=32:di=4;96:ex=1;35:*.py=1;31"
 export PS1="\[\e[0;35m\]\n[\w]\[\[\e[m\] \[\e[0;37m\]\n\$\[\e[m\] "
 
 c() {
-  cd "$1" && ls --color=auto
+  cd "$1" && ls
 }
 
 
